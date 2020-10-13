fa614dd952bac81a34be490aecdae2016b3a704c
Zenzlia <natalie.hong@uqconnect.edu.au>
2020-10-13 18:59:17 +1000

    Passing 4/5... subject to change in like an hour lol??

--- HW4 Support Files/src/FactChecker.java
+++ HW4 Support Files/src/FactChecker.java
@@ -1,6 +1,35 @@
-import java.util.List;
+import java.util.*;
 
 public class FactChecker {
+
+    private static class Node {
+        Set<String> presentPeople;
+
+        String next;
+
+        private Node(String person1, String person2) {
+            this.presentPeople = new HashSet<>();
+            this.presentPeople.add(person1);
+            this.presentPeople.add(person2);
+        }
+
+        private Node(String person1) {
+            this.presentPeople = new HashSet<>();
+            this.presentPeople.add(person1);
+        }
+
+        private void addNext(String next) {
+            this.next = next;
+        }
+
+        private void addPerson(String person) {
+            this.presentPeople.add(person);
+        }
+
+        private boolean containsPerson(String person) {
+            return this.presentPeople.contains(person);
+        }
+    }
 
     /**
      * Checks if a list of facts is internally consistent. 
@@ -11,9 +40,44 @@ public class FactChecker {
      * @return true if all the facts are internally consistent, otherwise false.
      */
     public static boolean areFactsConsistent(List<Fact> facts) {
-        // TODO: implement this!
-        
-        return false;
-    }
+        LinkedList<Node> factTracker = new LinkedList<>();
+        HashMap<String, Node> factMap = new HashMap<>();
 
+        for (Fact fact: facts) {
+            Node current;
+            if (fact.getType() == Fact.FactType.TYPE_TWO) { // Person a and b were at the party at the same time. //TODO: Error Checking
+                if (factMap.containsKey(fact.getPersonA())) { // person A person is already established as a
+                    current = factMap.get(fact.getPersonA());
+                    if (current.next != null && current.next.equals(fact.getPersonB())) {
+                        return false;
+                    }
+                    current.addPerson(fact.getPersonB());
+                } else {
+                    current = new Node(fact.getPersonA(), fact.getPersonB());
+                    factTracker.add(current);
+                }
+
+                factMap.put(fact.getPersonB(), current);
+                factMap.put(fact.getPersonA(), current);
+
+            } else { // Person a left before person b arrived.
+                Node toAdd = new Node(fact.getPersonB());
+                if (factMap.containsKey(fact.getPersonA())) { // A came before B, B already in the map
+                    current = factMap.get(fact.getPersonA());
+                    if (current.containsPerson(fact.getPersonB())) {
+                        return false;
+                    }
+                } else {
+                    current = new Node(fact.getPersonA());
+                    factTracker.add(current);
+                    factTracker.add(toAdd);
+                }
+
+                current.addNext(fact.getPersonB());
+                factMap.put(fact.getPersonA(), current);
+                factMap.put(fact.getPersonB(), toAdd);
+            }
+        }
+        return true;
+    }
 }
--- HW4 Support Files/tests/FactCheckerTest.java
+++ HW4 Support Files/tests/FactCheckerTest.java
@@ -4,7 +4,17 @@ import java.util.List;
 import org.junit.Test;
 
 public class FactCheckerTest {
-    
+
+    /**
+     * Person a left before person b arrived.
+     * TYPE_ONE
+     */
+    /**
+     * Person a and b were at the party at the same time.
+     * TYPE_TWO
+     */
+
+
     @Test
     public void testSimpleInconsistent() {
         List<Fact> facts = List.of(
@@ -20,6 +30,16 @@ public class FactCheckerTest {
         List<Fact> facts = List.of(
                 new Fact(Fact.FactType.TYPE_ONE, "a", "b"),
                 new Fact(Fact.FactType.TYPE_ONE, "b", "c")
+        );
+
+        assertTrue(FactChecker.areFactsConsistent(facts));
+    }
+
+    @Test
+    public void testType2Consistent() {
+        List<Fact> facts = List.of(
+                new Fact(Fact.FactType.TYPE_TWO, "a", "b"),
+                new Fact(Fact.FactType.TYPE_TWO, "b", "c")
         );
 
         assertTrue(FactChecker.areFactsConsistent(facts));
